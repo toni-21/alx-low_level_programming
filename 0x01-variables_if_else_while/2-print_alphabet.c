297fdddb5a0843c2cf80bb7c9d610f9d4d09b12a
Toni <dannyore001@gmail.com>
2022-03-11 03:28:41 -0800

    scripts added

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,12 +4,16 @@
  *
  * Return: Always 0 (Success)
  */
-
-int main (void)
+int main(void)
 {
+
 	char c;
-	for(c = 'a'; c <= 'z' ; c++)
+
+	for (c = 'a'; c <= 'z'; c++)
+	{
 		putchar(c);
 		putchar('\n');
+	}
+
 return (0);
 }
