88369d738d6d0d72c19250f1cca491239915e73a
Toni <dannyore001@gmail.com>
2022-03-17 19:00:53 -0700

    minor changes

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,38 +1,22 @@
 #include "main.h"
+
 /**
- * print_number - prints a number
- * @n: Input number
+ * print_number - prints an integer
+ * @n: tracked var
  */
 
 void print_number(int n)
 {
-	int len, res, i, temp, expo;
+	unsigned int x = n;
 
-	res = n;
-	expo = len =  1;
-/*Check negatives*/
-	if (res < 0)
+	if (n < 0)
 	{
-		res *= -1;
-		_putchar('-');
+		_putchar(45);
+		x = -x;
 	}
-
-/**/
-	temp = res;
-	while (temp >= 10)
+	if ((x / 10) > 0)
 	{
-		len++;
-		temp /= 10;
+		print_number(x / 10);
 	}
-
-/*Create Exponent*/
-	for (i = 1; i < len; i++)
-		expo *= 10;
-/*Main */
-	while (expo > 1)
-	{
-		_putchar((res / expo) % 10 + '0');
-		expo /= 10;
-	}
-	_putchar(res % 10 + '0');
+	_putchar((x % 10) + 48);
 }
