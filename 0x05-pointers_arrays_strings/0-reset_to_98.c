087503006a42562850198d519eec2acdd4730afc
Toni <dannyore001@gmail.com>
2022-03-20 23:57:40 -0700

    minor changes

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -2,10 +2,11 @@
 /**
  * reset_to_98 - a function that takes a pointer
  *  to int 98
+ *  @c: takes in a pointer
  *  Return: (0)
  */
 
-void reset_to_98(int *n)
+void reset_to_98(int *c)
 {
-	*n = 98;
+	*c = 98;
 }
