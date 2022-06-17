f79087bae2759396aa7aac1031a099f4a9d8631b
Toni <dannyore001@gmail.com>
2022-06-17 04:41:20 -0700

    hot fix

---
+++ 0x18-dynamic_libraries/operations.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+/**
+ * add - adds two numbers
+ * @a:integer
+ * @b: integer
+ * Return: sum of a and b
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - subtracts two numbers
+ * @a:integer
+ * @b: integer
+ * Return: difference of a and b
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - multiplies two numbers
+ * @a:integer
+ * @b: integer
+ * Return: product of a and b
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - divides two numbers
+ * @a:integer
+ * @b: integer
+ * Return: division of a and b
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - gives the remainder when a number is divided by another number
+ * @a:integer
+ * @b: integer
+ * Return: the remainder
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
